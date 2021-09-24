0db1a81cac2c989fb86dccbf3ab39cd1a69cf11d
Cédric Bois <cedric.bois@uca.fr>
2021-09-24 12:49:57 +0200

    add title on page

--- assets/js/components/Page.js
+++ assets/js/components/Page.js
@@ -2,17 +2,30 @@ import React, { useEffect, useState } from 'react'
 
 import { useSelector } from 'react-redux'
 import { getPageByTag } from 'store/pages/pagesSlice'
+import { getMenuItems } from 'store/pages/menuSlice'
 
 const Page = ({ tag }) => {
   const pagesFilter = useSelector(getPageByTag)
 
+  const menuItems = useSelector(getMenuItems)
   const [page, setPage] = useState(null)
+  const [currentMenuItem, setCurrentMenuItem] = useState(null)
+
   useEffect(() => {
     setPage(pagesFilter(tag))
   }, [])
 
+  useEffect(() => {
+    if (menuItems != null && page != null) {
+      setCurrentMenuItem(
+        menuItems.find((item) => item.page != null && item.page.id === page.id)
+      )
+    }
+  }, [menuItems, page])
+
   return (
     <div className="page-container">
+      {currentMenuItem != null && <h1>{currentMenuItem.name} </h1>}
       {page != null && (
         <>
           {page.headingImg != null && (
